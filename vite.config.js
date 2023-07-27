d627544c9c48df3b62fcba9a84c79b52f5eb3e2e
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-07-27 23:03:25 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/front-tz/",
   plugins: [react()],
+  base: "/front-tz/",
 });
