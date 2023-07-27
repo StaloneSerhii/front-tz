1345c24e0164c4590a1232d97b8c30a21247a286
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-07-27 13:53:20 +0300

    redux

---
+++ src/redux/operations.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://nodejs-hw-goit-03-restapi.onrender.com";
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = "";
+  },
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (credentials) => {
+    console.log(credentials);
+    try {
+      const { data } = await axios.post(`/auth/register`, credentials);
+      token.set(data.token);
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
+export const logIn = createAsyncThunk("auth/login", async (credentials) => {
+  console.log(credentials);
+  try {
+    const { data } = await axios.post(`/auth/login`, credentials);
+    token.set(data.token);
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+export const logOut = createAsyncThunk("auth/logout", async () => {
+  try {
+    await axios.post(`/auth/logout`);
+    token.unset();
+  } catch (e) {
+    console.log(e);
+  }
+});
