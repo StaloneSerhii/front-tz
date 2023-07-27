0f695f07ed78b7552f1b4c2f8baa87bf1267846d
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-07-27 16:44:50 +0300

    фі

--- src/redux/operations.jsx
+++ src/redux/operations.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "http://localhost:3011/api";
+axios.defaults.baseURL = "https://nodejs-hw-goit-03-restapi.onrender.com/api";
 
 const token = {
   set(token) {
