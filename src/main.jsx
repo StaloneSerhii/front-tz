c98c7bfa94e298cba8fab626d643c49fddb2fc77
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-07-28 13:52:41 +0300

    xd

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
