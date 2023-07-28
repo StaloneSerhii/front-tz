0d335ebc64e40fe3885f44fc2b2f9fc1c3558d38
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-07-28 11:38:30 +0300

    l

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./redux/store.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/front-tz">
       <App />
     </BrowserRouter>
   </Provider>
