2dd50d26f3abb4eeb3747652efcd25f27e2073c6
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-07-28 12:25:04 +0300

    re

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,31 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import { PrivateRoute } from "./redux/privateRoute";
-import Main from "./pages/main";
-import { RestrictedRoute } from "./redux/restrikeRoute";
+import { useState } from "react";
+
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        width: "100%",
-      }}
-    >
-      <Routes>
-        <Route
-          path="/"
-          element={<RestrictedRoute redirectTo="/main" component={<Login />} />}
-        />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute redirectTo="/main" component={<Register />} />
-          }
-        />
-        <Route
-          path="/main"
-          element={<PrivateRoute redirectTo="/" component={<Main />} />}
-        />
-      </Routes>
-    </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 
